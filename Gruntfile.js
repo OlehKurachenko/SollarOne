23825d3779e0a114534b39c1b89e8f89f96609ea
OlehKurachenko <oleh.kurachenko@gmail.com>
2017-12-12 18:06:48 +0200

    system utils configured, header.ejs edited

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function (grunt) {
+    require('jit-grunt')(grunt);
+
+    var config = {
+        pkg: grunt.file.readJSON('package.json'),
+
+        browserify: {
+            options: {
+                transform: [require('brfs')],
+                browserifyOptions: {
+                    basedir: "Frontend/src/js/"
+                }
+            },
+            landing: {
+                src: 'Frontend/src/landing.js',
+                dest: 'Frontend/www/assets/js/landing.js'
+            },
+            constructor: {
+                src: 'Frontend/src/constructor.js',
+                dest: 'Frontend/www/assets/js/constructor.js'
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "Frontend/www/assets/css/common.css": "Frontend/www/assets/less/common.less",
+                    "Frontend/www/assets/css/landing.css": "Frontend/www/assets/less/landing.less",
+                    "Frontend/www/assets/css/constructor.css": "Frontend/www/assets/less/constructor.less"
+                }
+            }
+        }
+    };
+
+    var watchDebug = {
+        options: {
+            'no-beep': true
+        },
+        //Назва завдання будь-яка
+        scripts: {
+            //На зміни в яких файлах реагувати
+            files: ['Frontend/src/**/*.js', 'Frontend/**/*.ejs'],
+            //Які завдання виконувати під час зміни в файлах
+            tasks: ['browserify:pizza']
+        }
+    };
+
+
+    config.watch = watchDebug;
+    grunt.initConfig(config);
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // TODO configure below
+    grunt.registerTask('default',
+        [
+            //'browserify:landing',
+            //'browserify:constructor',
+            'less'
+        ]
+    );
+
+};
