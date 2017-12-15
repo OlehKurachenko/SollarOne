d1fa08706a5ea8dcdb00b54f52e161d1c2d47b71
OlehKurachenko <oleh.kurachenko@gmail.com>
2017-12-15 02:28:44 +0200

    completes range ready

--- Backend/data/completesList.js
+++ Backend/data/completesList.js
@@ -1,23 +1,6 @@
 var parts = require('./parts');
 
-exports.countCompletePrice = function (complete) {
-    var sum = 0;
-    if (complete.deck) {
-        if (complete.deck.shape)
-            sum += complete.deck.shape.price;
-        if (complete.deck.graphic)
-            sum += complete.deck.graphic.price;
-    }
-    if (complete.griptape)
-        sum += complete.griptape.price;
-    if (complete.trucks)
-        sum += complete.trucks.price;
-    if (complete.wheels)
-        sum += complete.wheels.price;
-    return sum;
-};
-
-exports.list = {
+module.exports = {
     JET_RACCON: {
         name_ua: "Реактивний Єнот",
         styles_ua: ["Фрірайд", "Даунхіл"],
@@ -27,8 +10,47 @@ exports.list = {
             graphic: parts.decks.graphic.STD_PRINT_2
         },
         griptape: parts.griptapes.griptape.CH_BLACK,
-        trucks: parts.trucks.BEAR_GRIZZLY_GEN5,
-        wheels: parts.wheels.HAWGS_MINIMONSTERS,
+        trucks_c: parts.trucks.trucks.BEAR_GRIZZLY_GEN5,
+        wheels_c: parts.wheels.wheels.HAWGS_MINIMONSTERS,
         picture_url: "assets/images/completes/complete_1.jpg"
+    },
+    DANCING_SNAKE: {
+        name_ua: "Танцююча змія",
+        styles_ua: ["Денсинг"],
+        deck: {
+            shape: parts.decks.shape.FLEX_FOX,
+            flex: parts.decks.flex.MEDIUM,
+            graphic: null
+        },
+        griptape: parts.griptapes.griptape.CH_WHITE,
+        trucks_c: parts.trucks.trucks.PARIS_V2_180,
+        wheels_c: parts.wheels.wheels.HAWGS_FATTIE_70,
+        picture_url: "assets/images/completes/complete_5.jpg"
+    },
+    FAU_RACCON: {
+        name_ua: "Потужний Єнот",
+        styles_ua: ["Фрірайд"],
+        deck: {
+            shape: parts.decks.shape.ROCCET_RACCON,
+            flex: parts.decks.flex.SOFT,
+            graphic: parts.decks.graphic.STD_PRINT_3
+        },
+        griptape: parts.griptapes.griptape.CH_BLACK,
+        trucks_c: parts.trucks.trucks.BEAR_GRIZZLY_GEN5,
+        wheels_c: parts.wheels.wheels.HAWGS_MINIMONSTERS,
+        picture_url: "assets/images/completes/complete_2.jpg"
+    },
+    BLUE_RACCON: {
+        name_ua: "Смішний Єнот",
+        styles_ua: ["Фрірайд", "Карвінг"],
+        deck: {
+            shape: parts.decks.shape.ROCCET_RACCON,
+            flex: parts.decks.flex.HARD,
+            graphic: parts.decks.graphic.STD_PRINT_2
+        },
+        griptape: parts.griptapes.griptape.CH_BLACK,
+        trucks_c: parts.trucks.trucks.BEAR_GRIZZLY_GEN5,
+        wheels_c: parts.wheels.wheels.HAWGS_MINIMONSTERS,
+        picture_url: "assets/images/completes/complete_3.jpg"
     }
 };
--- Frontend/src/completes/completesList.js
+++ Frontend/src/completes/completesList.js
@@ -28,12 +28,40 @@ function showCompletesList(filters) {
     }
 }
 
+function countCompletePrice(complete) {
+    var sum = 0;
+    if (complete.deck) {
+        if (complete.deck.shape)
+            sum += complete.deck.shape.price;
+        if (complete.deck.graphic)
+            sum += complete.deck.graphic.price;
+    }
+    if (complete.griptape)
+        sum += complete.griptape.price;
+    if (complete.trucks)
+        sum += complete.trucks.price;
+    if (complete.wheels)
+        sum += complete.wheels.price;
+    return sum;
+}
+
 function initiateCompletesList() {
     AJAX.get('api/getCompletesList', function (data) {
-        completesList = data.list;
+        completesList = data;
         completesListKeys = Object.keys(completesList);
         for (var i = 0; i < completesListKeys.length; ++i) {
-            completesListHTML[i] = Templates.completesList_oneItem({name: completesList[completesListKeys[i]].name_ua});
+            var val = completesList[completesListKeys[i]];
+            completesListHTML[i] = Templates.completesList_oneItem(
+                {
+                    picture: val.picture_url,
+                    name: val.name_ua,
+                    styles: val.styles_ua.join(', '),
+                    length: val.deck.shape.length,
+                    flex: val.deck.flex,
+                    trucks_ln: val.trucks_c.company + ' ' + val.trucks_c.model + ' ' + val.trucks_c.width,
+                    wheels_ln: val.wheels_c.company + ' ' + val.wheels_c.model + ' ' + val.wheels_c.size,
+                    price: countCompletePrice(val)
+                });
             completesListNodes[i] = $(completesListHTML[i]);
         }
         showCompletesList(filters);
